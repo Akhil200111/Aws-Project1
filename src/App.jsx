b119e622b4a77796f6bbad4e099bef6c5cb5b654
AK <aakhilp1@gmail.com>
2026-04-13 15:46:14 +0530

    Frontend Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to my Application</h1>
+    <div className='page'>
+      <h1 className='head'>Welcome to my Application</h1>
+      <p className='para'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam fugit praesentium ullam dolorum. Corporis quidem laboriosam architecto dolor ipsam aperiam, dignissimos autem eligendi vel at quis tempora inventore. Nisi, maxime.
+      Alias odio magni cum quaerat distinctio, repudiandae, in, voluptates modi beatae eligendi recusandae dolor nulla quo suscipit harum vel qui porro tenetur illum inventore incidunt cupiditate sequi pariatur? Natus, pariatur.
+      Quod explicabo laboriosam eos minus id numquam illo perspiciatis dolorem? Eveniet iure dolor commodi quidem ad quo ducimus, obcaecati dolorum ab placeat inventore veritatis eaque itaque consectetur cum ea? Ea.
+      Illo quia iure beatae aperiam totam id aut, recusandae eaque mollitia fugit quidem rem esse amet qui, dolorum similique necessitatibus voluptas tempore quae vitae harum. Reiciendis pariatur libero aperiam consequuntur!
+      Omnis adipisci, nobis facere quas tempora consectetur expedita, perspiciatis nulla ducimus assumenda aspernatur nam nihil maxime fuga sapiente culpa dolorem? Nostrum vel voluptatibus esse doloribus eaque suscipit maxime cupiditate voluptatum!</p>
+
+      <div className="btns">
+        <button>Explore Now</button>
+        <button>Contact US</button>
+      </div>
     </div>
   );
 }
